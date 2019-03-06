8e0e4855d45f2f34e9430bed26151634a78ac944
Blake A Long <blake.a.long@gmail.com>
2019-03-06 17:47:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeliLine = [];
 function takeANumber(katzDeliLine, newName) {
-  katzDeliLine.push(newName);
+  
     return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`;
   }
 
